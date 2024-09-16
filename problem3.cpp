4bb17848b1acc151a00f4100b2c7f79276df9701
o.goziyev <ogoziyev2005@gmail.com>
2024-09-16 18:40:41 +0500

    homework3

--- problem3.cpp
+++ problem3.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 int main(){
-    float a,b;
+    float a, b;
     cin>>a;
-    b=(a*9/5+32.0);
-    cout<<b<<endl;
+    b=((a*9/5)+32.0);
+    cout<<"The temperature in Farenheit is equal to "<<b<<endl;
     return 0;
 }
